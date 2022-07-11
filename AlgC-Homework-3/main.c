3b7e8096630c432c19a0bb7744b76a86aac1e2cc
Mertensoid <matsoukm@yandex.ru>
2022-07-11 17:52:22 +0300

    1. Выполнено задание 4. Добавлена сортировка выбором и вставками. Для 4х типов сортировки посчитано количество операций и выведено для сравнения с асимптотической сложностью

--- AlgC-Homework-3/main.c
+++ AlgC-Homework-3/main.c
@@ -10,8 +10,9 @@
 
 void solution1();
 void solution2();
-//void solution3();
-//void solution4();
+void solution3();
+void solution4();
+//void solution5();
 void menu();
 
 int main(int argc, const char * argv[]) {
@@ -30,11 +31,14 @@ void menu() {
         case 2:
             solution2();
             break;
-//        case 3:
-//            solution3();
-//            break;
-//        case 4:
-//            solution4();
+        case 3:
+            solution3();
+            break;
+        case 4:
+            solution4();
+            break;
+//        case 5:
+//            solution5();
 //            break;
         default:
             printf("Wrong solution number!");
@@ -145,14 +149,90 @@ int binarySearch(int *a) {
         middle = left + (right - left) / 2;
     }
     if (a[middle] == value) {
-        
+        return middle;
+    } else {
+        return -1;
     }
-    
-    return -1;
 }
 
 
 void solution3() {
     printf("Solution 3:\n");
     
+    int a[15] = {1, 3, 4, 5, 7, 10, 12, 16, 19, 21, 32, 34, 35, 67, 345};
+    int result = binarySearch(a);
+    printf("Result: %d\n", result);
+}
+
+int choiseSort(int *a) {
+    int count = 0;
+    int j = 0;
+    count++;
+    int jmin;
+    for (int i = 0; i < MaxN; i++) {
+        jmin = i;
+        count++;
+        for (j = i + 1; j < MaxN; j++) {
+            count++;
+            if (a[j] < a[jmin]) {
+                jmin = j;
+                count++;
+            }
+        }
+        count++;
+        swap(&a[i], &a[jmin]);
+    }
+    return count;
+}
+
+int insertionSort(int *a) {
+    int count = 0;
+    for (int i = 0; i < MaxN; i++) {
+        count++;
+        int currentNum = a[i];
+        count++;
+        int currentPos = i;
+        count++; // проверка условия1 цикла while
+        count++; // проверка условия2 цикла while
+        count++; // логическое И цикла while
+        while (currentPos > 0 && a[currentPos-1] > currentNum) {
+            count++;
+            swap(&a[currentPos], &a[currentPos-1]);
+            count++;
+            currentPos--;
+        }
+    }
+    return count;
+}
+
+void solution4() {
+    printf("Solution 4: \n");
+    printf("\n");
+    
+    printf("1. Bubble sort.\n");
+    int bubbleDifficulty = MaxN * MaxN;
+    printf("Estimated number of operations: %d\n", bubbleDifficulty);
+    solution1();
+    printf("\n");
+    
+    printf("2. Shaker sort.\n");
+    int shakerDifficulty = MaxN * MaxN;
+    printf("Estimated number of operations: %d\n", shakerDifficulty);
+    solution2();
+    printf("\n");
+    
+    printf("3. Choise sort.\n");
+    int choiseDifficulty = MaxN * MaxN;
+    printf("Estimated number of operations: %d\n", choiseDifficulty);
+    int a[15] = {5, 19, 3, 16, 34, 35, 21, 345, 34, 12, 5, 6, 4, 3, 67};
+    printf("Operations - %d\n", choiseSort(a));
+    printArr(a);
+    printf("\n");
+    
+    printf("4. Insertion sort.\n");
+    int insertionDifficulty = MaxN * MaxN;
+    printf("Estimated number of operations: %d\n", choiseDifficulty);
+    int b[15] = {5, 19, 3, 16, 34, 35, 21, 345, 34, 12, 5, 6, 4, 3, 67};
+    printf("Operations - %d\n", insertionSort(b));
+    printArr(a);
 }
